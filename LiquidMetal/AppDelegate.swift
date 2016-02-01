a48a1ce1ff6c394f18018b79f6e88377eb2c933e
dustin-biser <dustinbiser@gmail.com>
2016-01-31 22:12:24 -0500

    Initial Commit

---
+++ LiquidMetal/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LiquidMetal
+//
+//  Created by Dustin on 1/31/16.
+//  Copyright © 2016 none. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
